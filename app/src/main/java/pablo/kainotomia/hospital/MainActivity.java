252603c4872d1eadd486c83a9b775d467256bf1c
pablocontreras <pablo_contreras_1997@outlook.com>
2020-12-08 00:44:07 -0600

    Login

--- app/src/main/java/pablo/kainotomia/hospital/MainActivity.java
+++ app/src/main/java/pablo/kainotomia/hospital/MainActivity.java
@@ -1,14 +1,61 @@
 package pablo.kainotomia.hospital;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import pablo.kainotomia.hospital.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = binding.email.getText().toString();
+                String pass = binding.password.getText().toString();
+
+                if (email.equals("") || pass.equals("")) return;
+
+                signIn(email, pass);
+
+
+            }
+        });
+    }
+
+    private void signIn(String email, String pass) {
+        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent main = new Intent(MainActivity.this, MainActivity.class);
+                    main.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(main);
+                } else {
+                    Toast.makeText(MainActivity.this, "Credenciales incorrectas", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
